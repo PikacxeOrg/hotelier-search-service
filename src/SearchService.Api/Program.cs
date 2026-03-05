3bcfe9668d94da4eaa35daf678492b8ae101b1cd
Ilija Spasic <spasicilija1@gmail.com>
2026-03-05 02:15:26 +0100

    chore: add missing telemetry metrics nuget

--- src/SearchService.Api/Program.cs
+++ src/SearchService.Api/Program.cs
@@ -98,6 +98,8 @@ builder.Services.AddOpenTelemetry()
         .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
         .AddMeter("System.Net.Http")
         .AddMeter("System.Net.NameResolution")
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
         .AddPrometheusExporter())
     .WithTracing(tracing => tracing
         .AddAspNetCoreInstrumentation()
